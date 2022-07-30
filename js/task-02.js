b82b76abbd0bda46a14dd0e125de8cb8bbde2f88
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-07-30 18:12:21 +0300

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const itemsEl = ingredients.map((value) => {
+  const item = document.createElement("li");
+  item.textContent = value;
+  item.classList.add("item");
+  console.log(item);
+  return item;
+});
+
+listEl.append(...itemsEl);
