534aff4862da1eaf322f34fc4c53cbfd5ccbe393
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-07-31 19:47:06 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+const onFormSubmit = (event) => {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (!email.value || !password.value) {
+    alert("fuck data");
+  } else {
+    console.log({
+      email: email.value,
+      password: password.value,
+    });
+  }
+
+  event.currentTarget.reset();
+};
+
+form.addEventListener("submit", onFormSubmit);
