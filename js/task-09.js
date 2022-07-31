6921fcb09afca694da6ee6b9772c5f31dfab67d1
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-07-31 19:55:00 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector("body"),
+  buttonChangeColorEl: document.querySelector(".change-color"),
+  spanColorEl: document.querySelector(".color"),
+};
+
+const onChangeBgColorClick = () => {
+  const color = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = color;
+  refs.spanColorEl.textContent = color;
+};
+
+refs.buttonChangeColorEl.addEventListener("click", onChangeBgColorClick);
