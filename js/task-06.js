6b176aa561a438c13435cd75b1c146de68a2e229
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-07-31 18:48:26 +0300

    add task 6

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const input = document.querySelector("#validation-input");
+
+const DATA_LENGTH = input.getAttribute(["data-length"]);
+
+const replaceClass = (oldClass, newClass) => {
+  input.classList.remove(oldClass);
+  input.classList.add(newClass);
+};
+
+const onValidatorInput = () => {
+  input.value.length >= DATA_LENGTH
+    ? replaceClass("invalid", "valid")
+    : replaceClass("valid", "invalid");
+};
+
+input.addEventListener("blur", onValidatorInput);
