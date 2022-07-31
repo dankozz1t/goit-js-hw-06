fbceeca3cba1a8344a11cc09e36c08f9da7dfd5b
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-07-31 21:16:58 +0300

    update getRandomHexColor

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,10 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 const refs = {
